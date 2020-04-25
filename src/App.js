7df317f8bb8f32bccba044587c1afa29b0f9c3cb
iamwwc <qaq1362211689@gmail.com>
2020-04-25 20:37:18 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
-
+import Player from 'xgplayer'
+const log = (...args) => {
+  if (process.env.NODE_ENV === 'development')
+    console.log(...args)
+  return args
+}
+window.Player = Player
 function App() {
+  let src = ''
+  let xigua = useRef({})
+  let params = getParams(window.location.search.substr(1))
+  let playerurl = params.find(e => e[0] === 'playurl')
+  // 有url就不显示nav，默认没有url，显示
+  let show_nav = true
+  if (typeof playerurl !== 'undefined') {
+    playerurl[1] && (show_nav = false)
+    src = playerurl[1]
+  }
+  const handleClick = function () {
+    xigua.current.destroy(true)
+    xigua.current = new Player({
+      id: "xgplayer",
+      url: src,
+      autoplay: true,
+      width: '100%',
+      height: '100%'
+    })
+  }
+  useEffect(() => {
+    xigua.current = new Player({
+      id: "xgplayer",
+      width: '100%',
+      height: '100%'
+    })
+    if (!show_nav) {
+      xigua.current.start(src)
+      xigua.current.play()
+    }
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        show_nav && (
+          <div id="url-nav">
+            <input type="text" placeholder="play url" onChange={i => src = i.target.value}></input>
+            <button className="play-btn" onClick={handleClick}>播放</button>
+          </div>)
+      }
+      <div id="player" style={!show_nav ? {height:'100%',width:'100%'} :{}}>
+        <div id="xgplayer" style={{ paddingTop: 0, height: '100%' }}></div>
+      </div>
     </div>
   );
+}
+
+
+
+/**
+ * 
+ * @param {String} s 
+ */
+function getParams(s) {
+  let params = s.split('&')
+  return params.map(p => p.split('='))
 }
 
 export default App;
