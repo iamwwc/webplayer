8d3711a5549e85811f75c14e09793a4548618e3b
iamwwc <qaq1362211689@gmail.com>
2020-04-25 21:06:06 +0800

    添加分享、复制到剪贴板

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Player from 'xgplayer'
 const log = (...args) => {
@@ -10,10 +10,12 @@ window.Player = Player
 function App() {
   let src = ''
   let xigua = useRef({})
+  const divcopy = React.createRef()
   let params = getParams(window.location.search.substr(1))
   let playerurl = params.find(e => e[0] === 'playurl')
   // 有url就不显示nav，默认没有url，显示
   let show_nav = true
+  const [share, setShare] = useState('')
   if (typeof playerurl !== 'undefined') {
     playerurl[1] && (show_nav = false)
     src = playerurl[1]
@@ -39,16 +41,37 @@ function App() {
       xigua.current.play()
     }
   }, [])
+
+  const sharefullurl = () => {
+    setShare(`${window.location.href}?playurl=${src}`)
+  }
+  const copy = () => {
+    var range = document.createRange();
+    range.selectNode(divcopy.current); //changed here
+    window.getSelection().removeAllRanges(); 
+    window.getSelection().addRange(range); 
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  }
   return (
     <div className="App">
       {
         show_nav && (
-          <div id="url-nav">
-            <input type="text" placeholder="play url" onChange={i => src = i.target.value}></input>
-            <button className="play-btn" onClick={handleClick}>播放</button>
-          </div>)
+          <div className="nav-root">
+            <div id="url-nav">
+              <input type="text" placeholder="play url" onChange={i => src = i.target.value}></input>
+              <button className="play-btn" onClick={handleClick}>播放</button>
+            </div>
+            <div className=".share">
+              <button onClick={sharefullurl}>单页全屏</button>
+              <div ref={divcopy}>{share}</div>
+              {share !== '' && (<button onClick={copy}>复制</button>)}
+            </div>
+          </div>
+        )
+
       }
-      <div id="player" style={!show_nav ? {height:'100%',width:'100%'} :{}}>
+      <div id="player" style={!show_nav ? { height: '100%', width: '100%' } : {}}>
         <div id="xgplayer" style={{ paddingTop: 0, height: '100%' }}></div>
       </div>
     </div>
